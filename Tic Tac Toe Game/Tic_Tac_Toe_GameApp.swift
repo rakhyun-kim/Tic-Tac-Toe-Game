4dce9568dcdfeb919d154935375e99368031d1bb
RAKHYUN <kyle2598@gmail.com>
2023-10-05 12:09:40 -0500

    Initial Commit

---
+++ Tic Tac Toe Game/Tic_Tac_Toe_GameApp.swift
@@ -0,0 +1,17 @@
+//
+//  Tic_Tac_Toe_GameApp.swift
+//  Tic Tac Toe Game
+//
+//  Created by Rakhyun Kim on 10/5/23.
+//
+
+import SwiftUI
+
+@main
+struct Tic_Tac_Toe_GameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
